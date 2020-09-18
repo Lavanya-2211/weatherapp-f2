a6cd1138047e286abf6f198d991903ec3f53ad8c
Lavanya-2211 <lavanya86420@gmail.com>
2020-09-18 15:51:45 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,  VERSION } from '@angular/core';
+import { FormControl, FormGroup ,Validators } from '@angular/forms';
 import { WeatherService} from './weather.service';
 @Component({
   selector: 'my-app',
@@ -8,13 +9,28 @@ import { WeatherService} from './weather.service';
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   data: any;
+  cityform: FormGroup;
+  panel1 = false;
+  panel1data = false;
    constructor(private ws: WeatherService){}
    ngOnInit(){
-     this.ws.getData('vijayawada').subscribe( res => {
+     this.cityform = new FormGroup({
+       city: new FormControl(null,[Validators.required])
+     })
+   }
+
+   getData(){
+     let city = this.cityform.value.city;
+     this.ws.getData(city).subscribe( res => {
        console.log(res);
        this.data = res;
+       this.panel1 = false;
+       this.panel1data = true;
      }, err => {
        console.log(err);
      })
+   }
+   show1(){
+     this.panel1 = true;
    }
 }
